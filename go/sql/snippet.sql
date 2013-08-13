d65b80fba394ba6062cca7f19b53c9083fa01c18
Jeremy Brimeyer <jeremy@staticdouble.com>
2013-08-12 23:01:08 -0500

    Update snippet database table to include the base36 id of the snippet

--- go/sql/snippet.sql
+++ go/sql/snippet.sql
@@ -1,10 +1,12 @@
 CREATE TABLE "snippet" (
 	"snippet_id" INTEGER PRIMARY KEY,
+	"id_base36" TEXT,
 	"username" TEXT NOT NULL DEFAULT '',
 	"description" TEXT NOT NULL DEFAULT '',
 	"created" INTEGER NOT NULL DEFAULT 0,
 	"updated" INTEGER NOT NULL DEFAULT 0
 );
+CREATE INDEX "idx_snippet_id_base36" ON "snippet" ("id_base36");
 CREATE INDEX "idx_snippet_username" ON "snippet" ("username");
 CREATE INDEX "idx_snippet_created" ON "snippet" ("created");
 CREATE INDEX "idx_snippet_updated" ON "snippet" ("updated");
