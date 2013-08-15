fa0fea4a388b4ceef9d570450a6fae6b6f991529
Jeremy Brimeyer <jeremy@staticdouble.com>
2013-08-15 06:49:51 -0500

    Add git.go wrapper for libgit2 C library

---
+++ go/src/summa/git.go
@@ -0,0 +1,292 @@
+package summa
+
+/*
+#include <git2.h>
+#include <git2/errors.h>
+*/
+import "C"
+
+import (
+	"runtime"
+	"unsafe"
+)
+
+type IntBool bool
+type UIntBool bool
+
+type GitRepository struct {
+	ptr *C.git_repository
+}
+
+type GitIndex struct {
+	ptr *C.git_index
+}
+
+type GitOid struct {
+	bytes [20]byte
+}
+
+type GitError struct {
+	Message string
+	Code    int
+}
+
+func init() {
+	C.git_threads_init()
+}
+
+func (i IntBool) ToInt() C.int {
+	if i {
+		return C.int(1)
+	}
+	return C.int(0)
+}
+
+func (i UIntBool) ToInt() C.uint {
+	if i {
+		return C.uint(1)
+	}
+	return C.uint(0)
+}
+
+func (oid *GitOid) toC() *C.git_oid {
+	return (*C.git_oid)(unsafe.Pointer(&oid.bytes))
+}
+
+func (e *GitError) Error() string {
+	return e.Message
+}
+
+func GitErrorLast() error {
+	err := C.giterr_last()
+	if err == nil {
+		return &GitError{"No message", 0}
+	}
+	return &GitError{C.GoString(err.message), int(err.klass)}
+}
+
+func GitRepositoryInit(path string, bare UIntBool) (*GitRepository, error) {
+	repo := new(GitRepository)
+
+	cpath := C.CString(path)
+	defer C.free(unsafe.Pointer(cpath))
+
+	ret := C.git_repository_init(&repo.ptr, cpath, bare.ToInt())
+	if ret < 0 {
+		return nil, GitErrorLast()
+	}
+
+	runtime.SetFinalizer(repo, (*GitRepository).Free)
+	return repo, nil
+}
+
+func GitRepositoryOpen(path string) (*GitRepository, error) {
+	repo := new(GitRepository)
+
+	cpath := C.CString(path)
+	defer C.free(unsafe.Pointer(cpath))
+
+	ret := C.git_repository_open(&repo.ptr, cpath)
+	if ret < 0 {
+		return nil, GitErrorLast()
+	}
+
+	runtime.SetFinalizer(repo, (*GitRepository).Free)
+	return repo, nil
+}
+
+func (r *GitRepository) Free() {
+	runtime.SetFinalizer(r, nil)
+	C.git_repository_free(r.ptr)
+}
+
+func (r *GitRepository) Commit(name, email string) error {
+	var index *C.git_index
+	C.git_repository_index(&index, r.ptr)
+	defer C.git_index_free(index)
+
+	treeOid := new(C.git_oid)
+	C.git_index_write_tree(treeOid, index)
+
+	tree := new(C.git_tree)
+	C.git_tree_lookup(&tree, r.ptr, treeOid)
+	defer C.git_tree_free(tree)
+
+	signature := new(C.git_signature)
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+	cEmail := C.CString(email)
+	defer C.free(unsafe.Pointer(cEmail))
+	C.git_signature_now(&signature, cName, cEmail)
+	defer C.git_signature_free(signature)
+
+	headOid := new(C.git_oid)
+	cHead := C.CString("HEAD")
+	defer C.free(unsafe.Pointer(cHead))
+	ret := C.git_reference_name_to_id(headOid, r.ptr, cHead)
+
+	commitOid := new(C.git_oid)
+	cMessage := C.CString("")
+	defer C.free(unsafe.Pointer(cMessage))
+
+	if ret == 0 {
+		head := new(C.git_commit)
+		C.git_commit_lookup(&head, r.ptr, headOid)
+		defer C.git_commit_free(head)
+
+		parents := make([]*C.git_commit, 1)
+		parents[0] = head
+
+		C.git_commit_create(
+			commitOid,
+			r.ptr,
+			cHead,
+			signature,
+			signature,
+			nil,
+			cMessage,
+			tree,
+			1,
+			&parents[0])
+	} else {
+		C.git_commit_create(
+			commitOid,
+			r.ptr,
+			cHead,
+			signature,
+			signature,
+			nil,
+			cMessage,
+			tree,
+			0,
+			nil)
+	}
+
+	C.git_index_write(index)
+
+	return nil
+}
+
+func (r *GitRepository) Index() (*GitIndex, error) {
+	var ptr *C.git_index
+	ret := C.git_repository_index(&ptr, r.ptr)
+	if ret < 0 {
+		return nil, GitErrorLast()
+	}
+
+	idx := &GitIndex{ptr}
+	runtime.SetFinalizer(idx, (*GitIndex).Free)
+	return idx, nil
+}
+
+func (i *GitIndex) Free() {
+	runtime.SetFinalizer(i, nil)
+	C.git_index_free(i.ptr)
+}
+
+func (i *GitIndex) Write() error {
+	ret := C.git_index_write(i.ptr)
+	if ret < 0 {
+		return GitErrorLast()
+	}
+	return nil
+}
+
+func (i *GitIndex) WriteTree() (*GitOid, error) {
+	oid := new(GitOid)
+	ret := C.git_index_write_tree(oid.toC(), i.ptr)
+	if ret < 0 {
+		return nil, GitErrorLast()
+	}
+
+	return oid, nil
+}
+
+func (i *GitIndex) Add(file string) error {
+	cfile := C.CString(file)
+	defer C.free(unsafe.Pointer(cfile))
+
+	ret := C.git_index_add_bypath(i.ptr, cfile)
+	if ret < 0 {
+		return GitErrorLast()
+	}
+
+	return i.Write()
+}
+
+func (i *GitIndex) Rm(file string) error {
+	cfile := C.CString(file)
+	defer C.free(unsafe.Pointer(cfile))
+
+	ret := C.git_index_remove_bypath(i.ptr, cfile)
+	if ret < 0 {
+		return GitErrorLast()
+	}
+
+	return i.Write()
+}
+
+/*
+
+func GitInit(repoPath string) error {
+	_, err := git.InitRepository(repoPath, false)
+	return err
+}
+
+func GitCommitAll(repoPath string, u *User) error {
+	repo, err := git.OpenRepository(repoPath)
+	if err != nil {
+		return err
+	}
+
+	idx, err := repo.Index()
+	if err != nil {
+		return err
+	}
+
+	// TODO: stage all files here, including deletions
+	err = idx.AddByPath("index.html")
+	if err != nil {
+		return err
+	}
+
+	treeId, err := idx.WriteTree()
+	if err != nil {
+		return err
+	}
+
+	tree, err := repo.LookupTree(treeId)
+	if err != nil {
+		return err
+	}
+
+	// headRef, err := repo.LookupReference("HEAD")
+	// if err != nil {
+	// 	return err
+	// }
+
+	head, err := repo.GetLastCommit()
+	if err != nil {
+		return err
+	}
+
+	sig := &git.Signature{
+		Name:  u.DisplayName,
+		Email: u.Email,
+		When:  time.Now(),
+	}
+
+	_, err = repo.CreateCommit(
+		"HEAD",
+		sig,
+		sig,
+		"",
+		tree,
+		head,
+	)
+
+	idx.Write()
+
+	return err
+}
+*/
