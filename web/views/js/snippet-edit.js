777f99a5edb398dcfba75cb67ac8d1878923822f
Jeremy Brimeyer <jeremy@staticdouble.com>
2013-08-22 14:23:35 -0500

    Remove snippet-edit view files; re-using snippet-create for these

--- web/views/js/snippet-edit.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	var SnippetEditView = function SnippetEditView() {
-		this._super.constructor.call(this);
-		this.name = 'snippet-edit';
-	};
-	summa.inherit(summa.View, SnippetEditView);
-
-	summa.registerView(new SnippetEditView());
-})();
