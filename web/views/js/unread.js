38f6037a9f7f53738f5955d615047fe0b9b4740c
Jeremy Brimeyer <jeremy@staticdouble.com>
2013-08-22 21:33:55 -0500

    Implement the unread snippets view

--- web/views/js/unread.js
+++ web/views/js/unread.js
@@ -5,5 +5,25 @@
 	};
 	summa.inherit(summa.View, UnreadView);
 
+	/**
+	 * Render the view
+	 */
+	UnreadView.prototype.render = function render() {
+		var that = this;
+
+		summa.postToApi('/api/snippets/unread')
+			.done(function unreadLoadDone(json) {
+				that._super.render.call(
+					that,
+					{
+						snippets: json.data.snippets
+					}
+				);
+			})
+			.fail(function unreadLoadFail(jqXhr) {
+				summa.renderInlineView(jqXhr.status);
+			});
+	};
+
 	summa.registerView(new UnreadView());
 })();
